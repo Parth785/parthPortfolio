16d41852d86732623843fc5f909c61223926d191
Parth <parthlakhani112001@gmail.com>
2025-03-31 12:45:07 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';  // ✅ Import the component
+import { FormsModule } from '@angular/forms';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { SocialLinksComponent } from './social-links/social-links.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, HeroSectionComponent,SocialLinksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'portfolio-angular';
 }
